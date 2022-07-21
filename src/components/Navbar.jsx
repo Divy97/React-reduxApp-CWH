058423c7336faa342a40415b9beb3b3b3daf9ee3
Divy <divyparekh1810@gmail.com>
2022-07-21 16:36:05 +0530

    done witth accesing state in app

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const amount = useSelector((state) => state.amount);
+
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-light">
@@ -34,7 +37,7 @@ const Navbar = () => {
             </ul>
             <div>
               <button disabled={true} className="btn btn-primary">
-                Your Balance: 10000
+                Your Balance: {amount}
               </button>
             </div>
           </div>
