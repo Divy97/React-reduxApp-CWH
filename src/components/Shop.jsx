7b5d3147a26051c41e0a710792086820d3330834
Divy <divyparekh1810@gmail.com>
2022-07-21 16:03:10 +0530

    done eith the UI

---
+++ src/components/Shop.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <div>
+      <h2>DEPOSIT/WITHDRAW MONEY</h2>
+      <button className="btn btn-primary">-</button>
+      Update Balance
+      <button className="btn btn-primary">+</button>
+    </div>
+  );
+};
+
+export default Shop;
