0026226909cbf0775054c0743a2d21369693a255
Divy <divyparekh1810@gmail.com>
2022-07-21 16:45:26 +0530

    done witth updating state in app and complete project

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,12 +1,31 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "../state/index";
 
 const Shop = () => {
+  const dispatch = useDispatch();
+
+  const { withdrawMoney, depositMoney } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
   return (
     <div>
       <h2>DEPOSIT/WITHDRAW MONEY</h2>
-      <button className="btn btn-primary">-</button>
+      <button
+        className="btn btn-primary"
+        onClick={() => dispatch(withdrawMoney(100))}
+      >
+        -
+      </button>
       Update Balance
-      <button className="btn btn-primary">+</button>
+      <button
+        className="btn btn-primary"
+        onClick={() => dispatch(depositMoney(100))}
+      >
+        +
+      </button>
     </div>
   );
 };
